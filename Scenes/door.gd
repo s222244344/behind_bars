5a471390ab593244f3f5f80c8ce05fe12d284790
unknown <s22224434@deakin.edu.au>
2025-09-23 15:40:24 +1000

    initial commit

--- Scenes/door.gd
+++ Scenes/door.gd
@@ -1,12 +1,10 @@
 extends Node2D
-
 @export var required_key_id: String = "key_blue"
 @export var consume_key := true
 @export var temporary_open := false
 @export var reopen_after := 2.0
 
 var _player: Node = null
-
 @onready var _collider: CollisionShape2D = $StaticBody2D/CollisionShape2D
 @onready var _sprite: Sprite2D = $Sprite2D
 
@@ -15,37 +13,34 @@ func _ready() -> void:
 	$Area2D.body_exited.connect(_on_body_exited)
 	if _collider: _collider.disabled = false
 
-func _process(_delta: float) -> void:
+func _process(_d: float) -> void:
 	if _player and Input.is_action_just_pressed("interact"):
-		print("[DOOR] interact near door. Needs:", required_key_id)
 		_try_open()
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		_player = body
-		print("[DOOR] player entered door area")
+		print("Door armed for", body.name, "needs", required_key_id)
 
 func _on_body_exited(body: Node) -> void:
-	if body == _player:
-		_player = null
-		print("[DOOR] player left door area")
+	if body == _player: _player = null
 
 func _try_open() -> void:
 	if not _player: return
 	if _player.has_method("has_item") and _player.has_item(required_key_id, 1):
-		print("[DOOR] has key -> open")
 		if consume_key and _player.has_method("remove_item"):
 			_player.remove_item(required_key_id, 1)
 		_open()
 	else:
-		print("[DOOR] locked. Need:", required_key_id, " player has:",
-			(_player.has_method("has_item") and _player.has_item(required_key_id, 1)))
+		print("Locked:", name, "needs", required_key_id)
 
 func _open() -> void:
 	if _collider: _collider.disabled = true
-	if _sprite: _sprite.modulate.a = 0.35  # visual cue
+	if _sprite: _sprite.modulate.a = 0.35
+	print("Opened:", name)
 	if temporary_open:
-		get_tree().create_timer(reopen_after).timeout.connect(_relock)
+		var t := get_tree().create_timer(reopen_after)
+		t.timeout.connect(_relock)
 
 func _relock() -> void:
 	if _collider: _collider.disabled = false
--- inventory_ui.gd
+++ inventory_ui.gd
@@ -1,9 +1,7 @@
 extends CanvasLayer
-
 @onready var panel: Panel = $Panel
 @onready var list: ItemList = $Panel/VBoxContainer/ItemList
 @onready var title: Label = $Panel/VBoxContainer/Label
-
 var _player: Node = null
 
 func _ready() -> void:
@@ -13,25 +11,23 @@ func _ready() -> void:
 
 func _late_connect() -> void:
 	_player = get_tree().get_first_node_in_group("player")
-	print("[HUD] found player =", _player)
+	print("HUD: found player =", _player)
 	if _player and _player.has_signal("inventory_changed"):
 		_player.inventory_changed.connect(_update_list)
 		_update_list(_player.inventory)
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("inventory"):
+func _input(_e: InputEvent) -> void:
+	if Input.is_action_just_pressed("inventory"): # 'I'
 		panel.visible = not panel.visible
 		if panel.visible and _player:
 			_update_list(_player.inventory)
 
 func _update_list(inv: Dictionary) -> void:
 	list.clear()
-	var ids: Array = inv.keys()
-	ids.sort()  # Array.sort() in-place
+	var ids := inv.keys()
+	ids.sort()
 	for id in ids:
-		var amount: int = int(inv[id])
-		list.add_item("%s x%d" % [_pretty_name(str(id)), amount])
-	print("[HUD] inventory now:", inv)
+		list.add_item("%s x%d" % [_pretty_name(str(id)), int(inv[id])])
 
 func _pretty_name(id: String) -> String:
 	match id:
